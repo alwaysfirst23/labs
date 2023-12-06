a1271742d190ad6e78b148488e031958e420eaea
alwaysfirst23 <153075039+alwaysfirst23@users.noreply.github.com>
2023-12-06 13:50:41 +0400

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,111 @@
+﻿#include <tabulate/table.hpp>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void find_rect(vector<vector<int>> matrix, int row, int col) {
+    int square = 0;
+    int start_x = 1;
+	int start_y = 1;
+	int end_x = 1;
+    int end_y = 1;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            int local_i = i;
+            int local_j = j;
+            int lenght = 0;
+            int width = 1;
+            int local_square = 0;
+
+            while ((local_j < col) && (matrix[local_i][local_j] == 0)) {
+                local_j++;
+            }
+
+            lenght = local_j - j;
+            // Длина строки = local_j - j
+            // Далее перебираем строки
+            local_square = (lenght) * (width);
+            if (local_square > square) square = local_square;
+            while (lenght > 0) {
+                // Рассматриваем следующую строку
+                local_i++;
+                width++;
+                // Сдвигаем указатель обратно на позицию i
+                local_j = j;
+				// Пытаемся найти в нём строку из нулей длины length
+				// Если не получается - отбрасываем local_j - j нулей
+                for (local_j; ((local_j < col) && (local_j < j+lenght) && (local_i < row) && (matrix[local_i][local_j] == 0)); local_j++);
+                if (local_j - j != lenght) {
+					lenght -= lenght - (local_j - j); 
+                }
+                // Проверяем, не является ли текущая фигура наибольшей
+                local_square = (lenght) * (width);
+                if (local_square > square) {
+                    square = local_square;
+                    start_x = i + 1;
+                    start_y = j + 1;
+                    end_x = start_x + width - 1;
+                    end_y = start_y + lenght - 1;
+                }
+            }
+        }
+    }
+    cout << "Square: " << square << endl;
+	cout << "Upper left corner: " << start_x << ", " << start_y << endl;
+	cout << "Down right corner: " << end_x << ", " << end_y << endl;
+}
+
+int row_col_enter() {
+    int number;
+    while (!(cin >> number) || (cin.peek() != '\n') || (number < 2)) {
+        cout << "Enter an integer >1: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+    return number;
+}
+
+void matrix_enter(vector<vector<int>>& matrix, int row, int col) {
+    cout << "Enter numbers (only 0 or 1)" << endl;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            int elem;
+            while (!(cin >> elem) || cin.peek() != '\n' || (elem != 0 && elem != 1)) {
+                cout << "Incorrect input, try again: ";
+                cin.clear();
+                cin.ignore(10000, '\n');
+            }
+            matrix[i][j] = elem;
+        }
+    }
+}
+
+void matrix_print(vector<vector<int>>& matrix, int row, int col) {
+    cout << endl << "Entered matrix: " << endl;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    char cont = 'Y';
+    do {
+        cout << "Enter the number of rows: ";
+        int row = row_col_enter();
+        cout << "Enter the number of columns: ";
+        int col = row_col_enter();
+        vector<vector<int>> matrix(row, std::vector<int>(col, 0));
+
+        matrix_enter(matrix, row, col);
+
+        matrix_print(matrix, row, col);
+
+        find_rect(matrix, row, col);
+
+        cout << "[Y] for continue: ";
+        cin >> cont;
+    } while (cont == 'Y');
+}
---
+++ 4.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void file_to_vector(vector<string>& lines, string& filename) {
+    string v;
+    cout << "Enter the name of file: ";
+    cin >> filename;
+    ifstream file_to_read(filename);
+    if (!file_to_read.is_open()) cout << "Error: file was not found" << endl;
+
+    while (!file_to_read.eof()) {
+        getline(file_to_read, v, '\n');
+        lines.push_back(v); 
+    }
+    file_to_read.close(); 
+}
+
+void redactor(vector<string>& lines, string& filename, string& act) {
+    int line;
+    string text;
+
+    cout << "What would you like to do with file? [view/edit/exit]:  ";
+    cin >> act;
+    if (act == "view") {
+        cout << "Which line do you want to view?: ";
+        while (!(cin >> line) || cin.peek() != '\n' || line < 1 || line > lines.size()) {
+            cout << "Incorrect line, try again: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        cout << lines[line - 1] << endl; 
+    }
+    else if (act == "edit") {
+        cout << "Which line do you want to edit?: ";
+        while (!(cin >> line) || cin.peek() != '\n' || line < 1 || line > lines.size()) {
+            cout << "Incorrect line, try again: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        cout << "Enter the text you want to put in the line: ";
+        cin.ignore(10000, '\n');
+        getline(cin, text);
+        lines[line - 1] = text;
+        ofstream file_to_write(filename);
+        for (int i = 0; i < lines.size(); i++) {
+            file_to_write << lines[i] << '\n';
+        }
+        file_to_write.close();
+    }
+}
+
+int main() {
+    string filename;
+    vector<string> lines;
+    string act;
+
+    file_to_vector(lines, filename);
+    do {
+        redactor(lines, filename, act);
+    } while ((act == "view") || (act == "edit"));
+}
